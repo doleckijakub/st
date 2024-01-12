c4bfb2284965c1ef0046afa63dbaab9237302043
Jakub Dolecki <doleckijakub@users.noreply.github.com>
2024-01-12 18:07:17 +0100

    Changed font, spaces per tab and opacity

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font:style=Regular:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -91,10 +91,10 @@ char *termname = "st-256color";
  *
  *	stty tabs
  */
-unsigned int tabspaces = 8;
+unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.7;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
